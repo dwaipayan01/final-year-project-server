a03ae20f2df5e8b9519d5d586b3e994f0e966bcc
dwaipayan01 <dwaipayanjoy1975@gmail.com>
2022-09-05 15:13:08 -0700

    my second commit

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 const express=require("express");
 const cors=require("cors");
 const app=express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port=process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qoriule.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.22hh7ow.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log("Connect hoise re");
-  // perform actions on the collection object
-  client.close();
+async function run(){
+  try{
+    await client.connect();
+    const packageCollection = client.db("lastProject").collection("packages");
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = packageCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+  });
+  app.get("/product/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const service = await packageCollection.findOne(query);
+    res.send(service);
 });
+  }
+  finally{
+
+  }
+}
+run().catch(console.dir);
 
 app.get("/",(req,res)=>{
     res.send("My versity project is running");
